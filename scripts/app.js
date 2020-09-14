4c12985d9cdb746025af70fcdf04c7ea07260066
jsans24 <jsans24@gmail.com>
2020-09-13 19:18:51 -0700

    Commit 1 - Verbal Questions

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+// ----------- Verbal Questions
+// 1. A parameter is a placeholder name you put into a function and an argument is the actual variable you put in when you call the function.
+// 2. Using return will effect the code (ex. changing the value of a variable) while console.log only prints to the console for you to see.
+// 3. The implications of a functions ability to return a value are that a function becomes a powerful tool to alter code and make things happen.
