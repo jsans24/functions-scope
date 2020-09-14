f599f799a2dba711ad1520f719ed2d7700ebeacd
jsans24 <jsans24@gmail.com>
2020-09-13 20:09:58 -0700

    Commit 5 - Sum Array

--- scripts/app.js
+++ scripts/app.js
@@ -33,3 +33,15 @@ function calculateSide(sideA, sideB) {
     return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2))
 }
 console.log(calculateSide(8, 6));
+
+// ----------- Sum Array
+function sumArray(arr) {
+    let split = [...arr]
+    let sum = 0
+    for (i = 0; i < split.length; i++) {
+        sum += split[i]
+    }
+    return sum
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
