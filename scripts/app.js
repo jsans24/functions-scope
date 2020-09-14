0b0993cff2fb1bdb4c03b129e922be4bc2dacff4
jsans24 <jsans24@gmail.com>
2020-09-13 19:31:00 -0700

    Commit 2 - Palindrome

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,17 @@
 // 1. A parameter is a placeholder name you put into a function and an argument is the actual variable you put in when you call the function.
 // 2. Using return will effect the code (ex. changing the value of a variable) while console.log only prints to the console for you to see.
 // 3. The implications of a functions ability to return a value are that a function becomes a powerful tool to alter code and make things happen.
+
+// ----------- Palindrome Again
+function checkPalindrome(a) {
+    let caps = a.toUpperCase()
+    let split = [...caps]
+    let reverse = split.reverse().join('')
+    console.log(caps)
+    console.log(reverse)
+    return caps === reverse
+}
+console.log(checkPalindrome("Radar"));
+console.log(checkPalindrome("Borscht"));
+console.log(checkPalindrome("racecar"));
+
